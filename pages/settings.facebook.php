b9c099b257f210bcd62f558bc410d2be9454978c
Thomas Skerbis <skerbis@klxm.de>
2019-05-31 15:06:25 +0200

    $this -> $addon

--- pages/settings.facebook.php
+++ pages/settings.facebook.php
@@ -9,36 +9,36 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
+$addon = rex_addon::get('feeds');
 $func = rex_request('func', 'string');
 
 if ($func == 'update') {
-    $this->setConfig(rex_post('settings', [
+    $addon->setConfig(rex_post('settings', [
         ['facebook_app_title', 'string'],
         ['facebook_app_id', 'string'],
         ['facebook_app_secret', 'string'],
     ]));
 
-    echo \rex_view::success($this->i18n('settings_saved'));
+    echo \rex_view::success($addon->i18n('settings_saved'));
 }
 
 $content = '';
 
 $formElements = [];
 $n = [];
-$n['label'] = '<label for="facebook-app-title">' . $this->i18n('facebook_app_title') . '</label>';
-$n['field'] = '<input class="form-control" type="text" id="facebook-app-title" name="settings[facebook_app_title]" value="' . htmlspecialchars($this->getConfig('facebook_app_title')) . '" />';
-$n['note'] = $this->i18n('facebook_app_title_note');
+$n['label'] = '<label for="facebook-app-title">' . $addon->i18n('facebook_app_title') . '</label>';
+$n['field'] = '<input class="form-control" type="text" id="facebook-app-title" name="settings[facebook_app_title]" value="' . htmlspecialchars($addon->getConfig('facebook_app_title')) . '" />';
+$n['note'] = $addon->i18n('facebook_app_title_note');
 $formElements[] = $n;
 
 $n = [];
-$n['label'] = '<label for="facebook-app-id">' . $this->i18n('facebook_app_id') . '</label>';
-$n['field'] = '<input class="form-control" type="text" id="facebook-app-id" name="settings[facebook_app_id]" value="' . htmlspecialchars($this->getConfig('facebook_app_id')) . '" />';
+$n['label'] = '<label for="facebook-app-id">' . $addon->i18n('facebook_app_id') . '</label>';
+$n['field'] = '<input class="form-control" type="text" id="facebook-app-id" name="settings[facebook_app_id]" value="' . htmlspecialchars($addon->getConfig('facebook_app_id')) . '" />';
 $formElements[] = $n;
 
 $n = [];
-$n['label'] = '<label for="facebook-app-secret">' . $this->i18n('facebook_app_secret') . '</label>';
-$n['field'] = '<input class="form-control" type="text" id="facebook-app-secret" name="settings[facebook_app_secret]" value="' . htmlspecialchars($this->getConfig('facebook_app_secret')) . '" />';
+$n['label'] = '<label for="facebook-app-secret">' . $addon->i18n('facebook_app_secret') . '</label>';
+$n['field'] = '<input class="form-control" type="text" id="facebook-app-secret" name="settings[facebook_app_secret]" value="' . htmlspecialchars($addon->getConfig('facebook_app_secret')) . '" />';
 $formElements[] = $n;
 
 $fragment = new \rex_fragment();
@@ -60,7 +60,7 @@ $buttons = $fragment->parse('core/form/submit.php');
 
 $fragment = new \rex_fragment();
 $fragment->setVar('class', 'edit', false);
-$fragment->setVar('title', $this->i18n('facebook_settings'), false);
+$fragment->setVar('title', $addon->i18n('facebook_settings'), false);
 $fragment->setVar('body', $content, false);
 $fragment->setVar('buttons', $buttons, false);
 $section = $fragment->parse('core/page/section.php');
