7b8e4fdbbd3b6d0a567607b22ebeebd175029412
Alexander Walther <github.com@alexplus.de>
2019-11-23 17:41:39 +0100

    WIP: Facebook Events auslesen
    Benötigt Feedback in #71 von @simonsinus wg. Abruf der Bilddatei.

---
+++ lib/stream/facebook_events.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * This file is part of the Feeds package.
+ *
+ * @author FriendsOfREDAXO
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+class rex_feeds_stream_facebook_events extends rex_feeds_stream_abstract
+{
+    public function getTypeName()
+    {
+        return rex_i18n::msg('feeds_facebook_events');
+    }
+
+    public function getTypeParams()
+    {
+        return [
+            [
+                'label' => rex_i18n::msg('feeds_facebook_events_page_name'),
+                'name' => 'page_name',
+                'type' => 'string',
+                'notice' => rex_i18n::msg('feeds_facebook_events_page_name_notice'),
+            ],
+            [
+                'label' => rex_i18n::msg('feeds_facebook_events_access_token'),
+                'name' => 'access_token',
+                'type' => 'string',
+                'notice' => rex_i18n::msg('feeds_facebook_events_access_token_notice'),
+            ],
+        ];
+    }
+
+    public function fetch()
+    {
+        $params = $this->typeParams;
+
+        try {
+            $socket = rex_socket::factory("graph.facebook.com", 443, true);
+            $socket->setPath('/'.$params['page_name'].'/events/?fields=description,name,start_time,id,link&access_token='.$params['access_token'].'');
+            $response = $socket->doGet();
+            if ($response->isOk()) {
+                $result = json_decode($response->getBody());
+                if (!is_array($result->data)) {
+                    echo rex_view::error("Fehler beim Abruf. Sind die Daten korrekt?");
+                } else {
+                    foreach ($result->data as $event) {
+                        $item = new rex_feeds_item($this->streamId, $event->id);
+                        // $item->setContentRaw();
+                        $item->setTitle($event->name);
+                        $item->setContent($event->description);
+                        $item->setUrl('https://www.facebook.de/events/' . $event->id);
+                        $item->setDate(new DateTime($event->start_time));
+                        $item->setRaw(json_encode($event));
+
+                        $item->setMedia('https://graph.facebook.com/'.$event->id.'/picture?type=large');
+                        $this->updateCount($item);
+                        $item->save();
+                    }
+                }
+            }
+        } catch (rex_socket_exception $e) {
+            echo rex_view::error($e->getMessage());
+        }
+    }
+}
