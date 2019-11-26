40440b495018d714ea2b5397513a4688fa97ef26
Alexander Walther <github.com@alexplus.de>
2019-11-27 00:16:19 +0100

    follow redirects for google places photo sync

--- lib/helper.php
+++ lib/helper.php
@@ -20,7 +20,7 @@ class rex_feeds_helper
      */
     public static function getDataUri($url)
     {
-        $response = rex_socket::factoryUrl($url)->doGet();
+        $response = rex_socket::factoryUrl($url)->followRedirects(2)->doGet();
         $mimeType = $response->getHeader('content-type');
         $uri = 'data:'.$mimeType;
 
